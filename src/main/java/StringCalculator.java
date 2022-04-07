7abd150de8793a4e405a47ce276c36c5d1466e04
Wolfero <ferorock44@gmail.com>
2022-04-07 12:50:13 +0100

    🟢 test: ignore_all_not_integer_numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,6 +2,10 @@ public class StringCalculator {
 
     public int add(String input) {
         if (input.isEmpty()) return 0;
-        return Integer.parseInt(input);
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            return 0;
+        }
     }
 }

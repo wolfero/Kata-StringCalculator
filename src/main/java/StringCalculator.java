af4ee22bca8af67bb44df4d60201e6d6f1f3af00
Wolfero <ferorock44@gmail.com>
2022-04-07 13:09:55 +0100

    🟢 test: sum_the_numbers_separated_by_commas

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,13 +2,16 @@ public class StringCalculator {
 
     public int add(String input) {
         if (input.isEmpty()) return 0;
-        return ignoreNotIntegerNumbers(input);
+        int firstSeparator = input.indexOf(",");
+        if (firstSeparator == -1) return ignoreNotIntegerNumbers(input);
+        String[] splitInput = input.split(",");
+        return ignoreNotIntegerNumbers(splitInput[0]) + add(splitInput[1]);
     }
 
     private int ignoreNotIntegerNumbers(String input) {
         try {
             return Integer.parseInt(input);
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             return 0;
         }
     }

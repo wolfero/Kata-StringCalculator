83a4b50320fb44c0aeda1c97ac14d0156edc0dcd
Wolfero <ferorock44@gmail.com>
2022-04-07 13:13:28 +0100

    ♻ Refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,11 +1,16 @@
 public class StringCalculator {
+    private final String defaultSeparator=",";
 
     public int add(String input) {
         if (input.isEmpty()) return 0;
-        int firstSeparator = input.indexOf(",");
+        int firstSeparator = firstSeparatorIndex(input);
         if (firstSeparator == -1) return ignoreNotIntegerNumbers(input);
-        String[] splitInput = input.split(",");
+        String[] splitInput = input.split(defaultSeparator);
         return ignoreNotIntegerNumbers(splitInput[0]) + add(splitInput[1]);
+    }
+
+    private int firstSeparatorIndex(String input) {
+        return input.indexOf(defaultSeparator);
     }
 
     private int ignoreNotIntegerNumbers(String input) {

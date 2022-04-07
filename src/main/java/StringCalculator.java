76184998cf1b95a87695902c3c6644120e4612b9
Wolfero <ferorock44@gmail.com>
2022-04-07 17:20:59 +0100

    ♻ Refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,18 +1,14 @@
 import java.util.Arrays;
 
 public class StringCalculator {
-    private final String defaultSeparator=",|\\n";
+    private final String defaultSeparator = ",|\\n";
 
     public int add(String input) {
         if (input.isEmpty()) return 0;
         String[] splitInput = input.split(defaultSeparator);
-        String firstNumber=splitInput[0];
-        String[] rest= Arrays.copyOfRange(splitInput,1,splitInput.length);
+        String firstNumber = splitInput[0];
+        String[] rest = Arrays.copyOfRange(splitInput, 1, splitInput.length);
         return ignoreNotIntegerNumbers(firstNumber) + sum(rest);
-    }
-
-    private int firstSeparatorIndex(String input) {
-        return input.indexOf(defaultSeparator);
     }
 
     private int ignoreNotIntegerNumbers(String input) {
@@ -23,10 +19,10 @@ public class StringCalculator {
         }
     }
 
-    private  int sum(String[] restInput){
-        String firstNumber=restInput[0];
-        if(restInput.length==1)return ignoreNotIntegerNumbers(firstNumber);
-        String[] rest= Arrays.copyOfRange(restInput,1,restInput.length);
+    private int sum(String[] restInput) {
+        String firstNumber = restInput[0];
+        if (restInput.length == 1) return ignoreNotIntegerNumbers(firstNumber);
+        String[] rest = Arrays.copyOfRange(restInput, 1, restInput.length);
         return ignoreNotIntegerNumbers(firstNumber) + sum(rest);
     }
 
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -1,17 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class StringCalculatorShould {
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void init() {
+        stringCalculator = new StringCalculator();
+    }
+
     @ParameterizedTest
     @CsvSource({
             "'',0"
     })
-    void return_zero_if_given_empty_string(String input,int output){
-        StringCalculator stringCalculator = new StringCalculator();
+    void return_zero_if_given_empty_string(String input, int output) {
+        stringCalculator = new StringCalculator();
 
-        int result=stringCalculator.add(input);
+        int result = stringCalculator.add(input);
 
         assertThat(result).isEqualTo(output);
     }
@@ -20,10 +28,10 @@ public class StringCalculatorShould {
     @CsvSource({
             "'1,a',1"
     })
-    void ignore_all_not_integer_numbers(String input,int output){
-        StringCalculator stringCalculator = new StringCalculator();
+    void ignore_all_not_integer_numbers(String input, int output) {
+        stringCalculator = new StringCalculator();
 
-        int result=stringCalculator.add(input);
+        int result = stringCalculator.add(input);
 
         assertThat(result).isEqualTo(output);
     }
@@ -33,10 +41,10 @@ public class StringCalculatorShould {
             "'1,2',3",
             "'1,2,1',4"
     })
-    void sum_the_numbers_separated_by_commas(String input,int output){
-        StringCalculator stringCalculator = new StringCalculator();
+    void sum_the_numbers_separated_by_commas(String input, int output) {
+        stringCalculator = new StringCalculator();
 
-        int result=stringCalculator.add(input);
+        int result = stringCalculator.add(input);
 
         assertThat(result).isEqualTo(output);
     }
@@ -46,10 +54,10 @@ public class StringCalculatorShould {
             "'1\n2',3",
             "'1,2\n1',4"
     })
-    void sum_the_numbers_separated_by_commas_and_new_lines(String input,int output){
-        StringCalculator stringCalculator = new StringCalculator();
+    void sum_the_numbers_separated_by_commas_and_new_lines(String input, int output) {
+        stringCalculator = new StringCalculator();
 
-        int result=stringCalculator.add(input);
+        int result = stringCalculator.add(input);
 
         assertThat(result).isEqualTo(output);
     }

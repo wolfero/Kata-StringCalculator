c661d3b286807e0de9ab0b01d26ac9a8e1b897d2
Wolfero <ferorock44@gmail.com>
2022-04-07 13:15:54 +0100

    🔴 test: sum_the_numbers_separated_by_commas_and_new_lines

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -18,7 +18,8 @@ public class StringCalculatorShould {
 
     @ParameterizedTest
     @CsvSource({
-            "'a',0"
+            "'a',0",
+            "'1,a',1"
     })
     void ignore_all_not_integer_numbers(String input,int output){
         StringCalculator stringCalculator = new StringCalculator();
@@ -30,9 +31,23 @@ public class StringCalculatorShould {
 
     @ParameterizedTest
     @CsvSource({
-            "'1,2',3"
+            "'1,2',3",
+            "'1,2,1',3"
     })
     void sum_the_numbers_separated_by_commas(String input,int output){
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int result=stringCalculator.add(input);
+
+        assertThat(result).isEqualTo(output);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'1\n2',3",
+            "'1,2\n1',3"
+    })
+    void sum_the_numbers_separated_by_commas_and_new_lines(String input,int output){
         StringCalculator stringCalculator = new StringCalculator();
 
         int result=stringCalculator.add(input);

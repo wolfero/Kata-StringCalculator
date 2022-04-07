b7481163cc5d2c70b8e0592b2c1030cb7f8cb79c
Wolfero <ferorock44@gmail.com>
2022-04-07 12:53:02 +0100

    ♻ Refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,10 +2,16 @@ public class StringCalculator {
 
     public int add(String input) {
         if (input.isEmpty()) return 0;
+        return ignoreNotIntegerNumbers(input);
+    }
+
+    private int ignoreNotIntegerNumbers(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e){
             return 0;
         }
     }
+
+
 }
